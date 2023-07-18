06f1750b21445ff7ebc6f5c806e65e7db40bb159
jordansmyth <jordan.smyth@mortgagehub.io>
2023-07-18 11:42:25 +0100

    added remaining buzz and fizzbuzz tests and functional logic

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,12 +1,38 @@
-const prod_code = (number) => {};
+const prod_code = (number) => {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else {
+    return number;
+  }
+};
 
-describe('Returns "Fizz" if the number is divisible by 3', () => {
-  it("", () => {
+describe("Fizzbuzz Tests", () => {
+  it("Returns Fizz if the number is divisible by 3", () => {
     // Arrange
     let number = 3;
     // Act
     const result = prod_code(number);
     // Assert
     expect(result).toEqual("Fizz");
+  });
+  it("Returns Buzz if the number is divisible by 5", () => {
+    //Arrange
+    let number = 5;
+    //Act
+    const result = prod_code(number);
+    //Assert
+    expect(result).toEqual("Buzz");
+  });
+  it("Returns FizzBuzz if the number is divisible by both 3 and 5", () => {
+    //Arrange
+    let number = 15;
+    //Act
+    const result = prod_code(number);
+    //Assert
+    expect(result).toEqual("FizzBuzz");
   });
 });
